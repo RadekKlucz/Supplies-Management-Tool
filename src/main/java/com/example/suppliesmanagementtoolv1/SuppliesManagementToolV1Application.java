e80dc8af5f285a99473eb41a88917d094fb4900f
Radosław Kluczewski <radek.info.klucz@gmail.com>
2024-02-27 19:38:37 +0100

    New structure of project has been created

---
+++ src/main/java/com/example/suppliesmanagementtoolv1/SuppliesManagementToolV1Application.java
@@ -0,0 +1,13 @@
+package com.example.suppliesmanagementtoolv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliesManagementToolV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliesManagementToolV1Application.class, args);
+	}
+
+}
